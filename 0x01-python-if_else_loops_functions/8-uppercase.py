36560aab580d77b9f74b140153bf129243cd0f19
godima20 <godima20@gmail.com>
2022-08-23 08:51:35 +0000

    prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for l in str:
+        print("{}".format(chr(ord(1) - 32)
+            if (ord(1) >= ord(1) >= ord("a") and
+                ord(1) <= ord("z")) else 1), end="")
+    print()
