5d0e748d49e07f0af1429641336474a780f7f303
godima20 <godima20@gmail.com>
2022-08-23 08:04:46 +0000

    The last digit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.radiant(-10, 10)
+if (number > 0)
+    print("{:d} is positive".formart(number))
+elif (number < 0)
+    print("{:d} is negative".format(number))
+else:
+    print("{:d} is zero".format(number))
+
