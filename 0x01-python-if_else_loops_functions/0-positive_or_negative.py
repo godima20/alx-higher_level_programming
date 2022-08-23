2c4c0c258df309f38dd025d5c8c4a0bfb363cbe6
godima20 <godima20@gmail.com>
2022-08-23 08:15:34 +0000

    printing alphabet in lowercase

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.radiant(-10, 10)
-if (number > 0)
+if (number > 0):
     print("{:d} is positive".formart(number))
-elif (number < 0)
+elif (number < 0):
     print("{:d} is negative".format(number))
 else:
     print("{:d} is zero".format(number))
