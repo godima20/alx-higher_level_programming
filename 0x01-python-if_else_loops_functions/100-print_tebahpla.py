4ea5ff550fd2621d925973aefe7de92fa6d13e7d
godima20 <godima20@gmail.com>
2022-08-23 14:07:35 +0000

    alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') -1, -1):
     offset = 96 if i % 2 == 0 else 64
-        print("{}".format(chr(offset+i)), end="")
+    print("{}".format(chr(offset+i)), end="")
