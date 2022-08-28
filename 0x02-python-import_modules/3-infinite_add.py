18a3d4d21535ab557362943cbb776791ad387e97
godima20 <godima20@gmail.com>
2022-08-28 14:26:59 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def inf_add():
-    sum = 0
-
-    for index, num in enumerate(argv):
-        if (index > 0):
-            sum = sum + int(num)
-    print("{:d}".format(sum))
-
-
-
-    if __name__ == "__main__":
-        inf_add()
+if __name__ == '__main__':
+    import sys
+    s = 0
+    for i in range(1, len(sys.argv)):
+        s += int(sys.argv[i])
+    print(s)
