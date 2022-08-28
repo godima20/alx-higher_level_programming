b10fd0975a06cd878ef33d937fea32dca84d2eb5
godima20 <godima20@gmail.com>
2022-08-28 14:32:50 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-if __name__ == "__main__":
-    for index in dir(hidden_4):
-        if index[0:2] != "__":
-            print("{:s}".format(i))
+if __name__ == '__main__':
+    import hidden_4
+    for n in dir(hidden_4):
+        if not n.startswith('__'):
+            print(n)
