51ccd115957a5c4e546f7cd5d646528e1cfc5a8c
godima20 <godima20@gmail.com>
2022-08-29 09:30:47 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    s = 0
-    for i in range(1, len(sys.argv)):
-        s += int(sys.argv[i])
-    print(s)
+    
+answer = 0
+
+for i in range(1, len(sys.argv)):
+    answer += int(sys.argv[i])
+print("{:d}".format(answer))
