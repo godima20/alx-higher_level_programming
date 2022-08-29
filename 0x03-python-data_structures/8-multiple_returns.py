416484b5f5e8b971fbdc1ac648520dba43280862
godima20 <godima20@gmail.com>
2022-08-29 07:27:45 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,3 @@
-#!/usr/bin.python3
-
-
+#!/usr/bin/python3
 def multiple_returns(sentence):
-    """Returns the length of a string aand its first character."""
-    count = len(sentence)
-    if count == 0:
-        first = None
-        return count, first
-    else:
-        first = sentence[0]
-        return count, first
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
