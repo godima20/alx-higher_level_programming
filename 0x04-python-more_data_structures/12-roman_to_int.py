48f5fa44bc96882954a219cc16f03125d3b58a83
godima20 <godima20@gmail.com>
2022-08-31 14:35:25 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,32 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    aux roman_string
     sum = 0
-    for i in range(len(roman_string)):
-        if dic[aux[i]] > dic[aux[i - 1]] and i > 0:
-            sum += dic[aux[i]] - (2 * dic[aux[i - 1]])
-        else:
-            sum += dic[aux[i]]
-    return sum
+    flag = 0
+    for i in range(roman_string)):
+        if flag:
+            flag = 0
+            continue
+        for key in dic:
+            if roman_string[i] == key and i != len(roman_string) -1:
+                if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
+                    sum += 4
+                    flag = 1
+                elif roman_string[i] == 'I' and roman_string[i + 1] == 'X':
+                    sum += 9
+                    flag = 1
+                elif roman_string[i] == 'X' and roman_string[i + 1] == 'L':
+                    sum += 40
+                    flag = 1
+                elif roman_string[i] == 'X' and roman_string[i + 1] == 'C':
+                    sum += 90
+                    flag = 1
+                elif roman_string[i] == 'C' and roman_string[i + 1] == 'D':
+                    sum += 400
+                    flag = 1
+                elif roman_string[i] == 'C' and roman_string[i + 1] == 'M':
+                    sum += dic[key]
+                elif roman_string[i] == key:
+                    sum += dic[key]
+            return sum
+        return 0
