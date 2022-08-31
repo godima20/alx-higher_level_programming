e93e0b87ffcec7ddaf70a89a9886751d305d81d5
godima20 <godima20@gmail.com>
2022-08-31 12:16:33 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return (0)
-    nInt = 0
-    nRom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D', 500, 'M': 1000}
-    for in roman_string:
-        nInt = nInt + nTom[i]
-    return (nInt)
+    if not roman_string or type(roman_string) != str:
+        return 0
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    aux roman_string
+    sum = 0
+    for i in range(len(roman_string)):
+        if dic[aux[i]] > dic[aux[i - 1]] and i > 0:
+            sum += dic[aux[i]] - (2 * dic[aux[i - 1]])
+        else:
+            sum += dic[aux[i]]
+    return sum
