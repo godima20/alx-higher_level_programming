e6f783f2e47743d8dfefe73e22324e4682bbf80b
godima20 <godima20@gmail.com>
2022-08-31 14:51:35 +0000

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list == [] or my_list is None:
-        return 0
-    res1, res2 = 0, 0
-    for i, j in my_list:
-        res1 += i * j
-        res2 += j
-        return (res1 / res2)
+    result = 0
+    if my_list:
+        weight_sum = 0
+        summ = 0
+        for x in my_list:
+            weight_sum = weight_sum + x[1]
+        result = summ / weight_sum
+    return result
 
