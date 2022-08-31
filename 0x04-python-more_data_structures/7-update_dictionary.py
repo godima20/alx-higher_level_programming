a18351aff9e0572e5da340e68a857bd0897be00d
godima20 <godima20@gmail.com>
2022-08-31 11:49:37 +0000

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for key, value in sorted(a_dictionary.items()):
-        print ("{:s}: {}".format(key, value))
+    a_dictionary[key] = value
+    return a_dictionary
