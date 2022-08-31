2da780beb38801b216510077616e6a0d62a2fe71
godima20 <godima20@gmail.com>
2022-08-31 14:39:41 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
-        return 0
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    sum = 0
-    flag = 0
-    for i in range(roman_string)):
-        if flag:
-            flag = 0
-            continue
-        for key in dic:
-            if roman_string[i] == key and i != len(roman_string) -1:
+    if type(roman_string) == str:
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        sum = 0
+        flag = 0
+        for i in range(roman_string)):
+            if flag:
+                flag = 0
+                continue
+            for key in dic:
+                if roman_string[i] == key and i != len(roman_string) -1:
                 if roman_string[i] == 'I' and roman_string[i + 1] == 'V':
                     sum += 4
                     flag = 1
