52e27527e1fa7171d53db10d49967f70a378a4fc
godima20 <godima20@gmail.com>
2022-08-31 12:33:55 +0000

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x*x, row)), matrix))
+    if matrix is None:
+        return(None)
+    if not matrix:
+        return(None)
+    else:
+        for i in range(matrix):
+            for j in i:
+    return(newmatrix)
