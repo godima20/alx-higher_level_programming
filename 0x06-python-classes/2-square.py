ed8d8a89b199371176558ece034d62dd99fc6a94
godima20 <godima20@gmail.com>
2022-09-14 06:27:54 +0000

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Class Square '''
+
+
+class Square:
+    '''Represent Square'''
+
+    def __init__(self, size=0):
+        '''Initializes the data with error handling'''
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            self.__size = size
