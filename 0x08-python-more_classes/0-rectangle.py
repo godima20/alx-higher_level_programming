0ee6a9be7ee2af7cd6654e701f1a2a900e5b22d2
godima20 <godima20@gmail.com>
2022-09-19 11:43:02 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
-
 class Rectangle:
-    """Class that defines a rectangle"""
-
-
-    def __init__(self):
-        pass
+    """ empty class that defines a rectangle """
+    pass
