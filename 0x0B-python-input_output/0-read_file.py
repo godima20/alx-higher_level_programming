eb7ca3cedfc8ed55a556607df300968bb715218e
godima20 <godima20@gmail.com>
2022-09-27 09:28:00 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints stdout
+"""
+
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
