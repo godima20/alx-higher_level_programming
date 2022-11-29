36b80be7e16b07d8b2601d50ed523dd3f80d744b
godima20 <godima20@gmail.com>
2022-11-29 17:55:51 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 let i = parseInt(process.argv[2]);
 if (Number.isNaN(i)) {
-	console.log('Missing number of occurrences');
+	 console.log('Missing number of occurrences');
 } else {
   while (i > 0) {
-     console.log('C is fun');
-     i--;
+    console.log('C is fun');
+    i--;
   }
 }
