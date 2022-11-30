f593ea1914076abd176d370ab84c888b7f73c9f1
godima20 <godima20@gmail.com>
2022-11-30 16:40:37 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = parseInt(process.argv[2]);
 if (Number.isNaN(number)) {
-	console.log('Not a number');
-	} else {
-		console.log('My number: ' + number);
-		}
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
