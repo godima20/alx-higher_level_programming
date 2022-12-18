4f6782c65fe478ab9a79f69e613473f14e51faae
godima20 <godima20@gmail.com>
2022-12-18 13:19:46 +0000

    adding condition name == main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
 
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
